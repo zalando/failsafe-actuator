c8fb91a7c0db3812ec372a76dca119f508d5b9e3
Malte Pickhan <malte.pickhan@zalando.de>
2017-03-02 20:59:58 +0100

    Issue #4 Add possibility for endpoint enabling Respecting the properties endpoints.enabled endpoints.failsafe.enabled endpoints.failsafe.sensitive And Enabling/Disabling the endpoint accordingly

--- src/main/java/org/zalando/failsafeactuator/endpoint/FailsafeEndpoint.java
+++ src/main/java/org/zalando/failsafeactuator/endpoint/FailsafeEndpoint.java
@@ -1,18 +1,22 @@
 /**
- * The MIT License (MIT)
- * Copyright (c) 2016 Zalando SE
+ * The MIT License (MIT) Copyright (c) 2016 Zalando SE
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package org.zalando.failsafeactuator.endpoint;
 
 import net.jodah.failsafe.CircuitBreaker;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
 import org.springframework.stereotype.Component;
 import org.zalando.failsafeactuator.endpoint.domain.CircuitBreakerState;
@@ -36,8 +40,11 @@ public class FailsafeEndpoint extends AbstractEndpoint<List<CircuitBreakerState>
   private final CircuitBreakerRegistry circuitBreakerRegistry;
 
   @Autowired
-  public FailsafeEndpoint(final CircuitBreakerRegistry circuitBreakerRegistry) {
-    super(ENDPOINT_ID);
+  public FailsafeEndpoint(
+      final CircuitBreakerRegistry circuitBreakerRegistry,
+      @Value("${endpoints.failsafe.sensitive:true}") final boolean sensitive,
+      @Value("${endpoints.failsafe.enabled:endpoints.enabled}")final boolean enabled) {
+    super(ENDPOINT_ID, sensitive, enabled);
     this.circuitBreakerRegistry = circuitBreakerRegistry;
   }
 
@@ -53,8 +60,8 @@ public class FailsafeEndpoint extends AbstractEndpoint<List<CircuitBreakerState>
         //Memorize unreferenced breakers which need to be removed later on
         breakersToRemove.add(identifier);
       } else {
-        final CircuitBreakerState state = new CircuitBreakerState(identifier, breaker.isClosed(), breaker.isOpen(), breaker.getState().equals(
-                CircuitBreaker.State.HALF_OPEN));
+        final CircuitBreakerState state =
+            new CircuitBreakerState(identifier, breaker.isClosed(), breaker.isOpen(), breaker.getState().equals(CircuitBreaker.State.HALF_OPEN));
         breakerStates.add(state);
       }
     }

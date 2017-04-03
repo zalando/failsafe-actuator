022a4251a7f106c50db6b68971576e75cb18ed7c
Malte Pickhan <malte.pickhan@zalando.de>
2017-04-03 09:45:00 +0200

    Issue #5 Fix unit tests

--- src/test/java/org/zalando/failsafeactuator/endpoint/EndpointTest.java
+++ src/test/java/org/zalando/failsafeactuator/endpoint/EndpointTest.java
@@ -16,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;

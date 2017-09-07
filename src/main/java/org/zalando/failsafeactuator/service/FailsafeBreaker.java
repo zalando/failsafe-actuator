936710614b1f4a101fe5b8e718702038026d1740
pdaum <peter.daum@zalando.de>
2017-09-07 22:12:22 +0200

    remove unused import

--- src/main/java/org/zalando/failsafeactuator/service/FailsafeBreaker.java
+++ src/main/java/org/zalando/failsafeactuator/service/FailsafeBreaker.java
@@ -13,11 +13,8 @@ package org.zalando.failsafeactuator.service;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.util.Optional;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({METHOD, PARAMETER, FIELD})

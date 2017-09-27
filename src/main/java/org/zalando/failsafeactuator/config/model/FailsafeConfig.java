9c565d87780e150be32c6635ada444e219c7da45
pdaum <peter.daum@zalando.de>
2017-09-27 22:04:31 +0200

    replaced static initializer with constructor

--- src/main/java/org/zalando/failsafeactuator/config/model/FailsafeConfig.java
+++ src/main/java/org/zalando/failsafeactuator/config/model/FailsafeConfig.java
@@ -12,6 +12,7 @@ package org.zalando.failsafeactuator.config.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Duration;
 import java.util.Collection;
@@ -21,24 +22,31 @@ import java.util.Map;
 @Data
 public class FailsafeConfig {
 
-  private static final int DEFAULT_DELAY = 1;
-  private static final int DEFAULT_SUCCESS_THRESHOLD = 1;
-  private static final int DEFAULT_FAILURE_THRESHOLD = 20;
+  private static final int DEFAULT_DELAY_IN_MINUTES = 1;
+  private static final int DEFAULT_SUCCESS_THRESHOLD_VALUE = 1;
+  private static final int DEFAULT_FAILURE_THRESHOLD_VALUE = 20;
 
-  private final Breaker defaults = new Breaker() {{
-    setDelay(Duration.ofMinutes(DEFAULT_DELAY));
-    setSuccessThreshold(new Threshold(DEFAULT_SUCCESS_THRESHOLD));
-    setFailureThreshold(new Threshold(DEFAULT_FAILURE_THRESHOLD));
-  }};
+  private static final Threshold DEFAULT_SUCCESS_THRESHOLD = new Threshold(DEFAULT_SUCCESS_THRESHOLD_VALUE);
+  private static final Threshold DEFAULT_FAILURE_THRESHOLD = new Threshold(DEFAULT_FAILURE_THRESHOLD_VALUE);
+  private static final Duration DEFAULT_DELAY = Duration.ofMinutes(DEFAULT_DELAY_IN_MINUTES);
+
+  private final Breaker defaults = new Breaker(DEFAULT_DELAY, DEFAULT_SUCCESS_THRESHOLD, DEFAULT_FAILURE_THRESHOLD);
 
   private final Map<String, Breaker> breaker = new HashMap<>();
 
   @Data
+  @NoArgsConstructor
   public static class Breaker {
     private Duration timeout;
     private Duration delay;
     private Threshold successThreshold;
     private Threshold failureThreshold;
+
+    Breaker(Duration delay, Threshold successThreshold, Threshold failureThreshold) {
+      this.delay = delay;
+      this.successThreshold = successThreshold;
+      this.failureThreshold = failureThreshold;
+    }
   }
 
   @Data

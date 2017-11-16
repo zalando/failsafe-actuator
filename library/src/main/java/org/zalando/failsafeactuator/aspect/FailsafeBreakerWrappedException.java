be7f1e5434bbb8401059d790a539c19d09938e15
Malte Pickhan <malte.pickhan@zalando.de>
2017-11-16 09:56:49 +0100

    Fix future as return type in Aspect

--- library/src/main/java/org/zalando/failsafeactuator/aspect/FailsafeBreakerWrappedException.java
+++
@@ -1,7 +0,0 @@
-package org.zalando.failsafeactuator.aspect;
-
-public class FailsafeBreakerWrappedException extends RuntimeException {
-  public FailsafeBreakerWrappedException(Throwable throwable) {
-    super(throwable);
-  }
-}

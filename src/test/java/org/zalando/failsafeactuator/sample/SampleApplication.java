b2fda14247ee77d0995ef06af90dbc9f2cd11366
Malte Pickhan <malte.pickhan@zalando.de>
2017-12-15 09:20:37 +0100

    Add a nice banner on startup

--- src/test/java/org/zalando/failsafeactuator/sample/SampleApplication.java
+++ src/test/java/org/zalando/failsafeactuator/sample/SampleApplication.java
@@ -1,14 +1,22 @@
 package org.zalando.failsafeactuator.sample;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 @SpringBootApplication
 public class SampleApplication {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SampleApplication.class);
 
   public static void main(final String[] args) {
     new SpringApplicationBuilder(SampleApplication.class)
@@ -22,7 +30,13 @@ public class SampleApplication {
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
       String port = environment.getProperty("server.port");
-      String banner = "";
+      Resource resource = new ClassPathResource("banner-zalando.txt");
+      String banner = null;
+      try {
+        banner = IOUtils.toString(resource.getInputStream(), "UTF-8");
+      } catch (IOException e) {
+        LOGGER.error("Error while reading banner", e);
+      }
       banner += "Failsafe-Actuator sample applicaton is running!\n";
       banner += "\n";
       banner += "See the circuit breaker status:\n";

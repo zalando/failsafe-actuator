94e480e43bbb1da21c9eb2d34de15b06e31eee93
Willi Schönborn <willi.schoenborn@zalando.de>
2018-06-28 09:33:55 +0200

    Switched from ApplicationContext to Map<String, CircuitBreaker> directly

--- src/main/java/org/zalando/failsafeactuator/CircuitBreakersEndpoint.java
+++ src/main/java/org/zalando/failsafeactuator/CircuitBreakersEndpoint.java
@@ -1,12 +1,10 @@
 package org.zalando.failsafeactuator;
 
 import net.jodah.failsafe.CircuitBreaker;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
-import org.springframework.context.ApplicationContext;
 
 import javax.annotation.Nullable;
 import java.util.Map;
@@ -17,22 +15,22 @@ import static java.util.stream.Collectors.toMap;
 @Endpoint(id = "circuit-breakers")
 public class CircuitBreakersEndpoint {
 
-    private final ApplicationContext context;
+    private final Map<String, CircuitBreaker> breakers;
 
-    CircuitBreakersEndpoint(final ApplicationContext context) {
-        this.context = context;
+    CircuitBreakersEndpoint(final Map<String, CircuitBreaker> breakers) {
+        this.breakers = breakers;
     }
 
     @ReadOperation
     public Map<String, CircuitBreakerView> circuitBreakers() {
-        return getCircuitBreakers().entrySet().stream()
+        return breakers.entrySet().stream()
                 .collect(toMap(Map.Entry::getKey, entry -> toView(entry.getValue())));
     }
 
     @ReadOperation
     @Nullable
     public CircuitBreakerView circuitBreaker(@Selector final String name) {
-        final CircuitBreaker breaker = getCircuitBreaker(name);
+        final CircuitBreaker breaker = breakers.get(name);
 
         if (breaker == null) {
             return null;
@@ -44,7 +42,7 @@ public class CircuitBreakersEndpoint {
     @WriteOperation
     @Nullable
     public CircuitBreakerView transitionTo(@Selector final String name, final CircuitBreaker.State state) {
-        final CircuitBreaker breaker = getCircuitBreaker(name);
+        final CircuitBreaker breaker = breakers.get(name);
 
         if (breaker == null) {
             return null;
@@ -53,6 +51,10 @@ public class CircuitBreakersEndpoint {
         transitioner(state).accept(breaker);
 
         return toView(breaker);
+    }
+
+    private CircuitBreakerView toView(final CircuitBreaker breaker) {
+        return new CircuitBreakerView(breaker.getState());
     }
 
     private Consumer<CircuitBreaker> transitioner(final CircuitBreaker.State state) {
@@ -64,23 +66,6 @@ public class CircuitBreakersEndpoint {
             default:
                 // to be able to cover this branch
                 return CircuitBreaker::close;
-        }
-    }
-
-    private CircuitBreakerView toView(final CircuitBreaker breaker) {
-        return new CircuitBreakerView(breaker.getState());
-    }
-
-    private Map<String, CircuitBreaker> getCircuitBreakers() {
-        return context.getBeansOfType(CircuitBreaker.class, false, true);
-    }
-
-    @Nullable
-    private CircuitBreaker getCircuitBreaker(final String name) {
-        try {
-            return context.getBean(name, CircuitBreaker.class);
-        } catch (final NoSuchBeanDefinitionException e) {
-            return null;
         }
     }
 
--- src/main/java/org/zalando/failsafeactuator/FailsafeAutoConfiguration.java
+++ src/main/java/org/zalando/failsafeactuator/FailsafeAutoConfiguration.java
@@ -1,10 +1,15 @@
 package org.zalando.failsafeactuator;
 
+import net.jodah.failsafe.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.Map;
 
 @Configuration
 public class FailsafeAutoConfiguration {
@@ -12,8 +17,10 @@ public class FailsafeAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnEnabledEndpoint
-    public CircuitBreakersEndpoint circuitBreakersEndpoint(final ApplicationContext context) {
-        return new CircuitBreakersEndpoint(context);
+    public CircuitBreakersEndpoint circuitBreakersEndpoint(
+            @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+            @Autowired(required = false) @Nullable final Map<String, CircuitBreaker> breakers) {
+        return new CircuitBreakersEndpoint(breakers == null ? Collections.emptyMap() : breakers);
     }
 
 }

b69d700cb1c172c271490b03ac7549d29e67842f
Willi Schönborn <willi.schoenborn@zalando.de>
2018-07-03 08:11:23 +0200

    Renamed configuration

--- src/main/java/org/zalando/failsafeactuator/CircuitBreakersEndpointAutoConfiguration.java
+++ src/main/java/org/zalando/failsafeactuator/CircuitBreakersEndpointAutoConfiguration.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @Configuration
-public class FailsafeAutoConfiguration {
+public class CircuitBreakersEndpointAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
